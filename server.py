e448affa5f6f4520e125b0cd09c008ae8a69da27
Christopher Lopes <christopher.lopes@gmail.com>
2018-02-25 23:14:29 -0800

    Serving index.html and adjusted for mobile

--- server.py
+++ server.py
@@ -4,59 +4,52 @@ import SimpleHTTPServer
 import SocketServer
 import sounddevice as sd
 import numpy as np
-import time
+import time   
 from threading import Thread
 from urlparse import urlparse, parse_qs
-
+            
 q = Queue.Queue()
 keys = {'a':439.0, 'b':389.0, 'c': 339.0, 'd': 289.0, 'e': 239.0, 'f': 189.0}
-
+                
 def getQuerystringValue(qs, key, defaultValue):
         return qs[key][0] if qs.has_key(key) else defaultValue
-
-def worker():
-    while True:
-        qs = q.get()
-        volume = float(getQuerystringValue(qs, "volume", 1.0))          # range [0.0, 1.0]
-        duration = float(getQuerystringValue(qs, "length", 1.0))        # in seconds, may be float
-#/usr/bin/python2.7
-import Queue
-import SimpleHTTPServer
-import SocketServer
-import sounddevice as sd
-import numpy as np
-import time
-from threading import Thread
-from urlparse import urlparse, parse_qs
-
-q = Queue.Queue()
-keys = {'a':439.0, 'b':389.0, 'c': 339.0, 'd': 289.0, 'e': 239.0, 'f': 189.0}
-
-def getQuerystringValue(qs, key, defaultValue):
-        return qs[key][0] if qs.has_key(key) else defaultValue
-
-def worker():
-    while True:
+                
+def worker():   
+    while True: 
         qs = q.get()
         volume = float(getQuerystringValue(qs, "volume", 1.0))          # range [0.0, 1.0]
         duration = float(getQuerystringValue(qs, "length", 1.0))        # in seconds, may be float
         f = keys[getQuerystringValue(qs, "key", 'a')]                   # sine frequency, Hz, may be float
         fs = 44100                                                      # sampling rate, Hz, must be integer
-
+                    
         sample = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
         sd.play(volume*sample, fs)
         q.task_done()
-
+                    
 class Server(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        qs = parse_qs(urlparse(self.path).query)
-        q.put(qs)
-        self.protocol_version='HTTP/1.1'
-        self.send_response(200, 'OK')
-        self.send_header('Content-type', 'text/html')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-        self.wfile.write(bytes("Played"))
+        # Duplicate from source
+        # https://github.com/python/cpython/blob/13ad3b7a82bf56d803fbe48ee5df6c4b08986c78/Lib/http/server.py
+        def serveIndex(self):
+                f = self.send_head()
+                if f:
+                    try:
+                        self.copyfile(f, self.wfile)
+                    finally:
+                        f.close()
+
+        def triggerSound(self):
+                qs = parse_qs(urlparse(self.path).query)
+                q.put(qs)
+                self.protocol_version='HTTP/1.1'
+                self.send_response(200, 'OK')
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Cache-Control', 'no-cache')
+                self.send_header('Access-Control-Allow-Origin', '*')
+                self.end_headers()
+                self.wfile.write(bytes("Played"))
+
+        def do_GET(self):
+                self.serveIndex() if self.path.find('?') == -1 else self.triggerSound()
 
 
 def serve_forever(port):
